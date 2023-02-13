f92f618355a9abe2aab4e301c47c0eadb1bd50e0
fastpostya <fastpost@rambler.ru>
2023-02-14 00:06:02 +0300

    add main.py and utils/good.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from utils.goods import Goods
+
+
+def main():
+    item1 = Goods("Смартфон", 10000, 20)
+    item2 = Goods("Ноутбук", 20000, 5)
+    print(item1.calculate_amount())
+    print(item2.calculate_amount())
+    Goods.pay_rate = 0.8
+
+    item1.apply_discount()
+
+    print(item1.price)
+    print(item2.price)
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/goods.py
@@ -0,0 +1,20 @@
+class Goods():
+    pay_rate=1.0
+    def __init__(self, name="", price=0.0, quantity=0):
+        self.name = name
+        self.price = price
+        #self.pay_rate = pay_rate
+        #self.price_discount = price
+        self.quantity = quantity
+
+    def __repr__(self) -> str:
+        text = ""
+        for dic in self.__dict__:
+            text += dic + "=" + str(self.__dict__[dic]) + ", "
+        return text[:-2]
+
+    def calculate_amount(self):
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
