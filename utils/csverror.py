5a7e6040ff507eff5db8463ec9842e027a70a14c
fastpostya <fastpost@rambler.ru>
2023-03-09 22:22:24 +0300

    changed readme.md and doc-strings

--- utils/csverror.py
+++ utils/csverror.py
@@ -2,6 +2,12 @@ class InstantiateCSVError(Exception):
     """Класс InstantiateCSVError для обработки исключения, возникающего
     при открытии csv-файла в случае, если в нем отсутствуют какие-либо
     необходимые столбцы.
+    Атрибуты:
+    -message:str - сообщение об ошибке
+
+    Методы:
+    -__init__-инициализация класса
+    -__str__- возвращает строку для печати с сообщением об ошибке
     """
     def __init__(self, message=""):
         if message:
@@ -10,5 +16,6 @@ class InstantiateCSVError(Exception):
             self.message = None
 
     def __str__(self):
+        """возвращает тип ошибки и сообщение"""
         return "InstantiateCSVError: " + str(self.message)
       
